d05f0f90d212551185195baa5f76e7a01fe66501
YeTheThe <gujiyu1983@icloud.com>
2024-12-13 11:56:24 +0800

    main.cpp added with base console

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxl=1000;//Man input lenth for console
+int main(){
+    char console[maxl];
+    memset(console,0,sizeof(console));
+    cout<<"Welcome to Simplecalc!"<<endl;
+    cout<<">";
+    scanf("%s",console);
+    return 0;
+}
